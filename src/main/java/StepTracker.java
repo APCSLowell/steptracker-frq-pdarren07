a2b0869d0b704a00e188c24dadfa3d03b3d33fff
pdarren07 <pdarren624@gmail.com>
2025-04-23 18:32:12 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -2,4 +2,29 @@ import java.util.ArrayList;
 public class StepTracker
 {
  /* to be implemented here */
+ private int numDays = 0;
+ private int total = 0;
+ private int min = 0;
+ private int totDays = 0;
+
+ public StepTracker(int active){
+  min = active;
+ }
+
+ public int addDailySteps(int steps){
+  if(steps >= min){
+   numDays++;
+  }
+  totDays++;
+  total = total + steps;
+ }
+
+ public int activeDays(){
+  return numDays;
+ }
+
+ public double averageSteps(){
+  private double avg = total/totDays;
+  return avg;
+ }
 } 
